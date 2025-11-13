319a122a2f31b418fe6dbb2f08bd0054228ffead
willo1301 <william.obrist@hotmail.fr>
2025-11-13 19:22:54 +0100

    Styled all the pages

--- Calendar_event/update.php
+++ Calendar_event/update.php
@@ -53,11 +53,50 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Update</title>
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <!-- Bootstrap CDN -->
+    <!-- Css -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Css -->
+    <!-- bootstrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- bootstrap icons -->
+
 </head>
 
 <body>
+
+    <div class="navbar">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">My Event Calendar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php"">Home</a>
+                        </li>
+                        <li class=" nav-item">
+                                <a class="nav-link active" aria-current="page" href="create.php"">Create Event</a>
+                        </li>
+                    </ul>
+                    <form class=" d-flex" role="search">
+                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="btn btn-outline-success" type="submit">Search</button>
+                                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+
+
+
 
     <div class="container">
         <div class="row">
@@ -101,6 +140,25 @@ if (isset($_POST['submit'])) {
         </div>
 
     </div>
+
+    <footer class="bg-dark py-5 mt-5">
+        <div class="container text-light text-center">
+            <p class="display-5 mb-3">Sport Calendar</p>
+            <a class="link-offset-2 link-underline link-underline-opacity-0" href="#"><i class="bi bi-facebook fs-2 me-3"></i></a>
+            <a class="link-offset-2 link-underline link-underline-opacity-0" href="#"><i class="bi bi-linkedin fs-2 me-3"></i></a>
+            <a class="link-offset-2 link-underline link-underline-opacity-0" href="#"><i class="bi bi-instagram fs-2 me-3"></i></a>
+            <a class="link-offset-2 link-underline link-underline-opacity-0" href="#"><i class="bi bi-pinterest fs-2 me-3"></i></a>
+            <br>
+            <br>
+            <small class="text-white-50">&copy; 2025 Copyright by Sport Calendar. All rights reserved</small>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+
+
+
+
 
 </body>
 
